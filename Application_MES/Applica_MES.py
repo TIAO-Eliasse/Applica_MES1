90a7b3975e2546eb87e19750e6464de4b4fc06c9
TIAO-Eliasse <eliassetiao@gmail.com>
2024-12-31 21:16:48 +0100

    Update Applica_MES.py

--- Application_MES/Applica_MES.py
+++ Application_MES/Applica_MES.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 
 st.write("Mecanisme de transmission des fluctuations des prix de pétrole dans l'économmie de la Guinée Equatoriale")
-data=pd.read_excel("C://Users//elias//Desktop//Application_MES//base_final.xlsx")
+data=pd.read_excel("C:/Users/elias/Desktop/Application_MES/base_final.xlsx")
 
 
 
